09ccfaeb95975c9e4ea4cc433dd4ff85f09e67f2
Gilbertoaleite <gilbertoaleite@hotmail.com>
2021-12-07 16:16:37 -0300

    configurado realizar login

--- js/index.js
+++ js/index.js
@@ -1,6 +1,36 @@
 /** @format */
-
 const myModal = new bootstrap.Modal('#register-modal');
+let logged = sessionStorage.getItem('logged');
+const session = localStorage.getItem('session');
+
+checkLogged();
+
+//logar no sistema
+document.getElementById('login-form').addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	const email = document.getElementById('email-input').value;
+	const password = document.getElementById('password-input').value;
+	const checkSession = document.getElementById('session-check').checked;
+
+	const account = getAccount(email);
+
+	if (!account) {
+		alert('Ops! Verifique o usúario ou a senha.');
+		return;
+	}
+
+	if (account) {
+		if (account.password !== password) {
+			alert('Ops! Verifique o usúario ou a senha.');
+			return;
+		}
+
+		saveSession(email, checkSession);
+
+		window.location.href = 'home.html';
+	}
+});
 
 //criar conta
 
@@ -15,7 +45,7 @@ document.getElementById('create-form').addEventListener('submit', function (e) {
 		return;
 	}
 
-	if (password.length < 5) {
+	if (password.length < 4) {
 		alert('Preencha a senha com no minimo 4 digitos.');
 		return;
 	}
@@ -27,9 +57,39 @@ document.getElementById('create-form').addEventListener('submit', function (e) {
 	});
 
 	myModal.hide();
+
 	alert('Conta criada com sucesso!');
 });
+//função salvar login e senha da sessão.
+function checkLogged() {
+	if (session) {
+		sessionStorage.setItem('logged', session);
+		logged = session;
+	}
+
+	if (logged) {
+		saveSession(logged, session);
+
+		window.location.href = 'home.html';
+	}
+}
 
 function saveAccount(data) {
 	localStorage.setItem(data.login, JSON.stringify(data));
+}
+function saveSession(data, saveSession) {
+	if (saveSession) {
+		localStorage.setItem('session', data);
+	}
+
+	sessionStorage.setItem('logged', data);
+}
+
+function getAccount(key) {
+	const account = localStorage.getItem(key);
+
+	if (account) {
+		return JSON.parse(account);
+	}
+	return '';
 }
